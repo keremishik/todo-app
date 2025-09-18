d108a55e9ec62b490a6ad54d73d9b427669e20c1
Kerem Isik <kerem.ishik@gmail.com>
2025-09-18 14:42:16 +0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+  <h1> Todo App </h1>
+  `
+})
+export class App {
+  
+}
