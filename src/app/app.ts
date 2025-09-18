524a36ec7874617de7137e0575434a7512ec5561
Kerem Isik <kerem.ishik@gmail.com>
2025-09-18 15:43:04 +0300

    implement get task and update task functinality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,66 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   template: `
   <h1> Todo App </h1>
+  <div>
+    <label>Task</label>
+    <input [(ngModel)]="task" />
+    <button (click)="saveTask()">Save</button>
+  </div>
+  <hr>
+  <div>
+    <label>Update Work</label>
+    <input [(ngModel)]="editTask" />
+    <button (click)="updateTask()">Update</button>
+  </div>
+  <div>
+    <ul>
+      @for(task of taskList; track task)
+      {
+        <li>
+          {{ 'Index: ' + $index + ' Task: ' + task }}
+          <button (click)="getTask($index)">Edit</button>
+          <button (click)="deleteTask($index)">Delete</button>
+
+        </li>
+      }
+    </ul>
+  </div>
   `
 })
 export class App {
-  
+
+  // properties
+  task: string = "";
+
+  taskList: string[] = [];
+
+  editTask: string = "";
+  editTaskId: number = 0;
+
+  // methods
+  saveTask () {
+    // add the task to the taskList
+    this.taskList.push(this.task);
+    this.task = "";
+  }
+
+  deleteTask (index: number) {
+    // remove the task from taskList
+    this.taskList.splice(index, 1);
+  }
+
+  getTask (index: number) {
+    this.editTask = this.taskList[index];
+    this.editTaskId = index;
+  }
+
+  updateTask () {
+    this.taskList[this.editTaskId] = this.editTask;
+  }
 }
